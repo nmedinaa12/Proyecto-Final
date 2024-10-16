a9e53d59caa4b5b7f1cb417351b7d3eef626e24c
Mariana Salgado Lopez <marisalgadolopez@gmail.com>
2024-10-16 16:44:59 -0500

    Creacion de paquetes y clases

--- src/autonoma/proyectoFinal/models/EmpleadoOperativo.java
+++ src/autonoma/proyectoFinal/models/EmpleadoOperativo.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package autonoma.proyectofinal.infertace;
+package autonoma.proyectoFinal.models;
 
 /**
  *
- * @author nmedi
+ * @author user
  */
-public class interface1 {
+public class EmpleadoOperativo {
     
 }
